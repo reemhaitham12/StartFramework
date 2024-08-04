970abb1026324f3ebfbc0213ccd534a5bd7a5a1a
reemhaitham12 <reemhaitham69@gmail.com>
2024-08-04 22:14:14 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base:"/StartFramework/",
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  base:"/Start-Framework/"
+  
 });
